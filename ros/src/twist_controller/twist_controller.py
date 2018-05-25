d531316fb7642cb471a22aba4d2ca1135c51a6ba
Trijeet Kr Modak <trijeetkr.modak@ad.infosys.com>
2018-05-25 11:46:05 +0530

    dbw update

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,11 +1,14 @@
+import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit,
+                 accel_limit, wheel_radius, wheel_base, steer_ratio, mac_lat_accel, max_steer_angle):
         # TODO: Implement
+        self.yaw_controller = YawController()
         pass
 
     def control(self, *args, **kwargs):
