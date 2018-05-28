a223b96e27874941ad05541911693adc0ce2663e
Trijeet Kr Modak <trijeetkr.modak@ad.infosys.com>
2018-05-28 09:53:57 +0530

    twist update

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -46,7 +46,7 @@ class Controller(object):
 
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
-        vel_error = linear_vel * current_vel
+        vel_error = linear_vel - current_vel
         self.last_vel = current_vel
 
         current_time = rospy.get_time()
